6c136a7c19ebebca661d130ba5e5852d6924fc14
John Gallego <johngallegodev@gmail.com>
2018-02-03 16:44:06 -0500

    add core module and header component

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { SharedModule } from '../shared/shared.module';
+import { HeaderComponent } from './header/header.component';
+
+@NgModule({
+  imports: [
+    SharedModule
+  ],
+  declarations: [
+    HeaderComponent
+  ],
+  exports: [
+    HeaderComponent
+  ]
+})
+export class CoreModule { }
