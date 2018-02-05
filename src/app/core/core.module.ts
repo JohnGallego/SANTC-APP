12b7c931de344ba7a615a0d8de5f1e479d3d6efd
John Gallego <johngallegodev@gmail.com>
2018-02-04 20:38:42 -0500

    add context nav bar component

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,18 +1,19 @@
 import { NgModule } from '@angular/core';
 import { SharedModule } from '../shared/shared.module';
 import { HeaderComponent } from './header/header.component';
+import { ContextNavComponent } from './context-nav/context-nav.component';
 
 @NgModule({
   imports: [
     SharedModule
   ],
   declarations: [
-    HeaderComponent
     HeaderComponent,
+    ContextNavComponent
   ],
   exports: [
-    HeaderComponent
     HeaderComponent,
+    ContextNavComponent
   ]
 })
 export class CoreModule { }
