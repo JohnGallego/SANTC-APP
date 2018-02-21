982489ab811cd8d14ef0b8e772f345b882b41c89
John Gallego <johngallegodev@gmail.com>
2018-02-20 21:51:38 -0500

    refactor: remove uneeded dependencies

--- src/app/shared/student-card/student-card.component.ts
+++ src/app/shared/student-card/student-card.component.ts
@@ -1,22 +1,16 @@
-import { Component, OnInit, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-student-card',
   templateUrl: './student-card.component.html',
   styleUrls: ['./student-card.component.scss']
 })
-export class StudentCardComponent implements OnInit, AfterViewInit {
-
-  @ViewChild('container') container: ElementRef;
+export class StudentCardComponent implements OnInit {
 
   constructor() {
   }
 
   ngOnInit() {
-  }
-
-  ngAfterViewInit() {
-
   }
 
 }
